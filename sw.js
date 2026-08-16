a720468211e1ce3b0a41d93789c47df4acf0ca1e
cybele0302-ux <cybele0302@gmail.com>
2026-08-16 20:17:25 +0800

    v29.52 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'wobench-v29.51';
+const VERSION = 'wobench-v29.52';
 const SHELL = ['./', './index.html', './app.js', './style.css', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png', './sw.js'];
 
 self.addEventListener('install', function (e) {
